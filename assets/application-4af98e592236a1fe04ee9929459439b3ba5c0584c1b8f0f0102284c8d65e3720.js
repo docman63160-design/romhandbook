cf9ad3ff7f83c5621f2f752058b50a6ef026f1d0
docman63160-design <docman63160@gmail.com>
2026-05-24 17:50:28 +0700

    Add files via upload

---
+++ assets/application-4af98e592236a1fe04ee9929459439b3ba5c0584c1b8f0f0102284c8d65e3720.js
@@ -0,0 +1,7 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails";
+import "controllers";
+
+import "lib/app";
+import "lib/color-mode";
+import "lib/search-modal";
